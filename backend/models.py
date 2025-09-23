3564079dc5af706336474cf0a44ec1234c3a6b53
jadchebly <159762508+jadchebly@users.noreply.github.com>
2025-09-23 14:06:02 +0200

    fixed db.py and added assesment model

---
+++ backend/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Float, Date
+
+Base = declarative_base()
+
+class Assessment(Base):
+    __tablename__ = "assessments"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    weight_pct = Column(Float, nullable=False)
+    due_date = Column(Date, nullable=False)
+    score_pct = Column(Float, nullable=True)
